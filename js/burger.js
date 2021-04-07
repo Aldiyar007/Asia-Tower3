cb0d25222407dbf320447ecf3ac47d2d82fa389c
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-04-07 22:44:06 +0600

    finish

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,23 @@
 document.querySelector('.header__burger').addEventListener("click", function (event) {
     document.querySelector('.header__burger').classList.toggle('active');
     document.querySelector('.header__menu').classList.toggle('active');
-    document.querySelector('body').classList.toggle('lock');
     document.querySelector('header').classList.toggle('not-opacity');
+    const body = document.querySelector('body');
+    disable();
+    enable();
+
+    function disable() {
+        let pagePosition = window.scrollY;
+        body.classList.add('lock');
+        body.dataset.position = pagePosition;
+        body.style.top = -pagePosition + 'px';
+    }
+
+    function enable() {
+        let pagePosition = parseInt(body.dataset.position, 10);
+        body.style.top = 'auto';
+        body.classList.remove('lock');
+        window.scroll({ top: pagePosition, left: 0 });
+        body.removeAttribute('data-position');
+    }
 });
--- js/slider.js
+++ js/slider.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         // autoplaySpeed: 2000,
         // draggable: false,
         // swipe: true,
-        // waitForAnumate: true,
+        // waitForAnimate: true,
         // responsive: [
         //     {
         //         breakpoint: 1200,
@@ -40,28 +40,84 @@ $(document).ready(function () {
         waitForAnimate: true,
         responsive: [
             {
-                breakpoint: 1371,
+                breakpoint: 1706,
                 settings: {
-                    arrows: false
+                    appendArrows: $('.slider-arrows')
                 }
             },
+            // {
+            //     breakpoint: 1371,
+            //     settings: {
+            //         arrows: false
+            //     }
+            // },
             {
-                breakpoint: 768,
+                breakpoint: 1020,
                 settings: {
-                    arrows: false,
+                    arrows: true,
+                    appendArrows: $('.slider-arrows'),
                     slidesToShow: 2
                 }
             },
             {
-                breakpoint: 550,
+                breakpoint: 662,
                 settings: {
                     slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    slidesToShow: 1,
+                    appendArrows: $('.slider-arrows')
                 }
             }
         ]
         // speed: 1000,
         // autoplaySpeed: 800
-    });
+    }),
+        $('.slider--two').slick({
+            arrows: true,
+            dots: true,
+            slidesToShow: 3,
+            autoplay: false,
+            adaptiveHeight: true,
+            slidesToScroll: 3,
+            autoplaySpeed: 2000,
+            draggable: false,
+            swipe: true,
+            speed: 800,
+            waitForAnimate: true,
+            responsive: [
+                {
+                    breakpoint: 1706,
+                    settings: {
+                        appendArrows: $('.slider-arrow')
+                    }
+                },
+                {
+                    breakpoint: 1020,
+                    settings: {
+                        arrows: true,
+                        appendArrows: $('.slider-arrow'),
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 662,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: 580,
+                    settings: {
+                        slidesToShow: 1,
+                        appendArrows: $('.slider-arrow')
+                    }
+                }
+            ]
+        });
     // $('.slider--two').slick({
     //     arrows: true,
     //     dots: true,

a5c50788a01ff954966462114d381e25baff3dd0
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-04-08 12:46:11 +0600

    del

--- js/smooth-scrolling.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#smooth-scrolling').on("click load resize", function (e) {
-        e.preventDefault();
-        var header = $('#header-first').offset().top;
-        $('html, body').animate({
-            scrollTop: header
-        }, 1000);
-    });
-});

b0c43f482418f8013fc66eb7162c20c5b613e990
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-04-12 20:44:18 +0600

    del

--- js/app.js
+++
@@ -1,9 +0,0 @@
-let textLoading = $('.text__loading');
-
-if (!$('.slider').slick) {
-    textLoading.text('У вас слабый интернет, слайдер загружается');
-}
-
-// if ($('.slider').slick) {
-//     textLoading.text('');
-// }

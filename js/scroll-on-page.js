8af1bb9a802199a69f83e8cccda8c6023cb2c9d9
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-04-21 11:00:11 +0600

    del

--- js/scroll-on-page.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function showHeader() {
-    var header = document.querySelector('header');
-
-    if (window.pageYOffset > 140) {
-        header.classList.add('active__scroll');
-    } else {
-        header.classList.remove('active__scroll');
-    }
-}

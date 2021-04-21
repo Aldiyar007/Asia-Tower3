4bbb9385802d372993dffc5652b3f52b3f13e80a
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-04-21 10:58:07 +0600

    finish

--- js/app.js
+++ js/app.js
@@ -4,3 +4,79 @@ let textLloading = $('.text__loading');
 if (slider.slick) {
     textLloading.text('');
 }
+
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        autoplay: true,
+        adaptiveHeight: true,
+        slidesToScroll: 3,
+        autoplaySpeed: 2000,
+        draggable: false,
+        swipe: true,
+        speed: 800,
+        waitForAnimate: true,
+        responsive: [
+            {
+                breakpoint: 1706,
+                settings: {
+                    appendArrows: $('.slider-arrows')
+                }
+            },
+            {
+                breakpoint: 1020,
+                settings: {
+                    arrows: true,
+                    slidesToScroll: 2,
+                    appendArrows: $('.slider-arrows'),
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 662,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1,
+                    appendArrows: $('.slider-arrows')
+                }
+            }
+        ]
+    });
+});
+
+$('.user-input').focus(function () {
+    $(this).parent().addClass("focus");
+}).blur(function () {
+    if ($(this).val() === '') {
+        $(this).parent().removeClass('focus');
+    }
+});
+
+function backToTop() {
+    let button = $('.back-to-top');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 200) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        $('html').animate({ scrollTop: 0 }, 1000);
+    });
+
+}
+
+backToTop();
---
+++ js/app2.js
@@ -0,0 +1,113 @@
+const body = document.querySelector('body');
+let headerBurger = document.querySelector('.header__burger');
+let numOffClicks = 0;
+
+function disable() {
+    let pagePosition = window.scrollY;
+    body.classList.add('lock');
+    body.dataset.position = pagePosition;
+    body.style.top = -pagePosition + 'px';
+}
+
+function enable() {
+    let pagePosition = parseInt(body.dataset.position, 10);
+    body.style.top = 'auto';
+    body.classList.remove('lock');
+    window.scroll({ top: pagePosition, left: 0 });
+    body.removeAttribute('data-position');
+}
+
+headerBurger.addEventListener("click", function () {
+    numOffClicks++;
+    if (numOffClicks % 2 !== 0) {
+        document.querySelector('.header__burger').classList.add('active');
+        document.querySelector('.header__menu').classList.add('active');
+        document.querySelector('header').classList.add('not-opacity');
+        disable();
+    } else {
+        document.querySelector('.header__burger').classList.remove('active');
+        document.querySelector('.header__menu').classList.remove('active');
+        document.querySelector('header').classList.remove('not-opacity');
+        enable();
+    };
+});
+
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+        let formData = new FormData(form);
+
+        if (error === 0) {
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                form.reset();
+                form.classList.remove('_sending');
+            } else {
+                alert('Ошибка');
+                form.classList.remove('_sending');
+            }
+        } else {
+            alert('Введите обязательные поля!');
+        }
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('.req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+});
+
+window.onscroll = function showHeader() {
+    var header = document.querySelector('header');
+
+    if (window.pageYOffset > 140) {
+        header.classList.add('active__scroll');
+    } else {
+        header.classList.remove('active__scroll');
+    }
+}

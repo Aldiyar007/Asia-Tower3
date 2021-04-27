6b5f4e84ea79bf44dc38cd5f92f00befd61daefd
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-04-27 20:00:56 +0600

    finish

--- js/app2.js
+++ js/app2.js
@@ -111,3 +111,23 @@ window.onscroll = function showHeader() {
         header.classList.remove('active__scroll');
     }
 }
+
+function wpguruLink() {
+    var istS = 'Источник:';
+    var copyR = '© Asia-Tower.kz';
+    var body_element = document.getElementsByTagName('body')[0];
+    var choose = window.getSelection();
+    var myLink = document.location.href;
+    var authorLink = "<br /><br />" + istS + ' ' + "<a href='" + myLink + "'>" + myLink + "</a><br />" + copyR;
+    var copytext = choose + authorLink;
+    var addDiv = document.createElement('div');
+    addDiv.style.position = 'absolute';
+    addDiv.style.left = '-99999px';
+    body_element.appendChild(addDiv);
+    addDiv.innerHTML = copytext;
+    choose.selectAllChildren(addDiv);
+    window.setTimeout(function () {
+        body_element.removeChild(addDiv);
+    }, 0);
+}
+document.oncopy = wpguruLink;

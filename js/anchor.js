4dabe63d9a3793ac613801c4c06fc05b90cd3324
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-05-03 09:52:28 +0600

    del

--- js/anchor.js
+++
@@ -1,7 +0,0 @@
-// /* Parallax */
-// let text = document.getElementsByClassName('intro__title');
-
-// window.addEventListener('scroll', function () {
-//     let value = window.scrollY;
-//     text.style.marginTop = value * 0 + 'px';
-// });

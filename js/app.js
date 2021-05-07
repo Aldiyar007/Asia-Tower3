a5318c810ad0b39bc67c8d07bc62d70112d0d814
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-05-07 11:55:57 +0600

    finish

--- js/app.js
+++ js/app.js
@@ -338,7 +338,7 @@ document.addEventListener('DOMContentLoaded', function () {
         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
     }
     function phoneTest(input) {
-        if (input.value.length < 11) {
+        if (input.value.length <= 10) {
             formAddError(input);
             error++;
         }

93e6f24981d9a905ccf7b0c3f58175c6fd031cf1
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-05-12 10:48:30 +0600

    аштшыр

--- js/app.js
+++ js/app.js
@@ -268,84 +268,82 @@ function Blur(x) {
 /* Form */
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('form');
-    if (form) {
-        form.addEventListener('submit', formSend);
+    form.addEventListener('submit', formSend);
 
-        async function formSend(e) {
-            e.preventDefault();
+    async function formSend(e) {
+        e.preventDefault();
 
-            let error = formValidate(form);
-            let formData = new FormData(form);
+        let error = formValidate(form);
+        let formData = new FormData(form);
 
-            if (error === 0) {
-                form.classList.add('_sending');
-                let response = await fetch('sendmail.php', {
-                    method: 'POST',
-                    body: formData
-                });
-                if (response.ok) {
-                    let result = await response.json();
-                    alert(result.message);
-                    form.reset();
-                    form.classList.remove('_sending');
-                } else {
-                    alert('Ошибка соединения. Повторите попытку позже');
-                    form.classList.remove('_sending');
-                }
+        if (error === 0) {
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                document.querySelectorAll('.user-input').reset();
+                form.classList.remove('_sending');
             } else {
-                alert('Введите обязательные поля корректно!');
+                alert('Ошибка соединения. Повторите попытку позже');
+                form.classList.remove('_sending');
             }
+        } else {
+            alert('Введите обязательные поля корректно!');
         }
+    }
 
-        function formValidate(form) {
-            let error = 0;
-            let formReq = document.querySelectorAll('.req');
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('.req');
 
-            for (let index = 0; index < formReq.length; index++) {
-                const input = formReq[index];
-                formRemoveError(input);
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
 
-                if (input.classList.contains('email')) {
-                    if (emailTest(input)) {
-                        formAddError(input);
-                        error++;
-                    }
-                } else if (input.classList.contains('phone')) {
-                    if (phoneTest(input)) {
-                        formAddError(input);
-                        error++;
-                    }
-                } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+            if (input.classList.contains('email')) {
+                if (emailTest(input)) {
                     formAddError(input);
                     error++;
-                } else {
-                    if (input.value === '') {
-                        formAddError(input);
-                        error++;
-                    }
                 }
-            }
-            return error;
-        }
-        function formAddError(input) {
-            input.parentElement.classList.add('_error');
-            input.classList.add('_error');
-        }
-        function formRemoveError(input) {
-            input.parentElement.classList.remove('_error');
-            input.classList.remove('_error');
-        }
-
-        function emailTest(input) {
-            return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-        }
-        function phoneTest(input) {
-            if (input.value.length <= 10) {
+            } else if (input.classList.contains('phone')) {
+                if (phoneTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
                 formAddError(input);
                 error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
             }
-            return !/^[0-9]+$/.test(input.value);
         }
+        return error;
+    }
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+    function phoneTest(input) {
+        if (input.value.length <= 10) {
+            formAddError(input);
+            error++;
+        }
+        return !/^[0-9]+$/.test(input.value);
     }
 });
 
